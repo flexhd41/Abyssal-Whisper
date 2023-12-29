6c9f87f57586410f18764c45f670df4cbaa4824c
flexhd <leck.mich@m-arsch.de>
2023-12-29 22:35:07 +0100

    Initial commit

---
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.kaupenjoe.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+
+	}
+}
